43491fc6c993791f7af5787e4bfd4c2fdc672892
Brian J Brennan <brianloveswords@gmail.com>
2013-12-18 13:08:27 -0500

    Can delete issuers.

--- app/routes/issuers.js
+++ app/routes/issuers.js
@@ -2,22 +2,25 @@ const Issuers = require('../models/issuer');
 
 exports = module.exports = function applyBadgeRoutes (server) {
 
-  server.get('/issuers', function showAllIssuers(req, res, next) {
+  server.get('/issuers', showAllIssuers);
+  server.get('/issuers/', showAllIssuers);
+  function showAllIssuers(req, res, next) {
     Issuers.get({}, function (error, rows) {
       if (error)
-        return next(error);
+        return handleError(error, null, res, next)
+
       res.send({issuers: rows.map(issuerFromDb)});
       return next();
-    })
-  });
+    });
+  };
 
-  server.get('/issuers/:issuerId', function showOneIssuer(req, res, next) {
-    const issuerId = req.params.issuerId;
-
+  server.get('/issuer/:issuerId', showOneIssuer);
+  server.get('/issuers/:issuerId', showOneIssuer);
+  function showOneIssuer(req, res, next) {
     const query = {slug: req.params.issuerId};
     Issuers.getOne(query, function (error, row) {
       if (error)
-        return next(error);
+        return handleError(error, row, res, next)
 
       if (!row) {
         res.send(404, {error: 'not found'});
@@ -26,41 +29,67 @@ exports = module.exports = function applyBadgeRoutes (server) {
 
       res.send({issuer: issuerFromDb(row)});
       return next();
-    })
-  });
+    });
+  };
 
-  server.post('/issuers', function saveIssuer(req, res, next) {
+  server.post('/issuers', saveIssuer);
+  server.post('/issuers/', saveIssuer);
+  function saveIssuer(req, res, next) {
     const row = fromPostToRow(req.body);
-    const errors = Issuers.validateRow(row);
+    const validationErrors = Issuers.validateRow(row);
 
-    if (errors.length) {
-      res.send(400, {errors: errors})
+    if (validationErrors.length) {
+      res.send(400, {errors: validationErrors})
       return next()
     }
 
     Issuers.put(row, function (error, result) {
-      if (error) {
-        const expected = knownError(error, row)
-        if (!expected) return next(error)
-        res.send.apply(res, expected)
+      if (error)
+        return handleError(error, row, res, next)
+
+      res.send(201, {status: 'created'})
+      return next();
+    });
+  };
+
+  server.del('/issuer/:issuerId', deleteIssuer);
+  server.del('/issuers/:issuerId', deleteIssuer);
+  function deleteIssuer(req, res, next) {
+    const query = {slug: req.params.issuerId};
+    Issuers.getOne(query, function (error, row) {
+      if (error)
+        return handleError(error, row, res, next)
+
+      if (!row) {
+        res.send(404, {error: 'not found'});
         return next()
       }
 
-      res.send({status: 'created'})
-      return next();
-    })
-  });
+      Issuers.del(row, function (error, result) {
+        if (error)
+          return handleError(error, row, req, next)
 
+        res.send({status: 'deleted', row: row});
+      })
+    });
+  };
 };
 
 const errorCodes = {
-  ER_DUP_ENTRY: [409, { error: 'An issuer with that `slug` already exists' }]
+  ER_DUP_ENTRY: [409, {error: 'An issuer with that `slug` already exists'}]
+}
+
+function handleError(error, row, res, next) {
+  const expected = knownError(error, row)
+  if (!expected) return next(error)
+  res.send.apply(res, expected)
+  return next()
 }
 
 function knownError(error, row) {
-  const err =  errorCodes[error.code];
+  const err = errorCodes[error.code];
   if (!err) return;
-  err[1].received = row
+  if (row) err[1].received = row
   return err;
 }
 
