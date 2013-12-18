259c9824967fb6222cf00165af82016d42a99ede
Brian J Brennan <brianloveswords@gmail.com>
2013-12-18 10:22:28 -0500

    Get issuers.

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,10 +1,12 @@
 var streamsql = require('streamsql');
 
-var db = streamsql.connect({
+var options = {
   driver: 'mysql',
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
-});
+}
+
+var db = streamsql.connect(options);
 
 exports = module.exports = db;
--- app/routes/issuers.js
+++ app/routes/issuers.js
@@ -1,18 +1,39 @@
-const Issuer = require('../models/issuer');
+const Issuers = require('../models/issuer');
 
 exports = module.exports = function applyBadgeRoutes (server) {
 
-  server.get('/issuers', function (req, res, next) {
-    Issuer.get({}, function (error, issuers) {
-      if (error) return next(error)
-      res.send({issuers:[]});
+  server.get('/issuers', function showAllIssuers(req, res, next) {
+    Issuers.get({}, function (error, rows) {
+      if (error) return next(error);
+      res.send({issuers: rows.map(issuerFromDb)});
       return next();
     })
   });
 
-  server.get('/issuers/:issuerId', function (req, res, next) {
-    res.send(req.params);
-    return next();
+  server.get('/issuers/:issuerId', function showOneIssuer(req, res, next) {
+    const issuerId = req.params.issuerId;
+
+    const query = {slug: req.params.issuerId};
+    Issuers.getOne(query, function (error, row) {
+      if (error) return next(error);
+
+      if (!row) {
+        res.send(404, {error: 'not found'});
+        return next()
+      }
+
+      res.send({issuer: issuerFromDb(row)});
+      return next();
+    })
   });
 
 };
+
+function issuerFromDb(row) {
+  return {
+    id: row.id,
+    slug: row.slug,
+    name: row.name,
+    email: row.email,
+  }
+}
