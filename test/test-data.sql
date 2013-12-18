edd35c0f79996fadd40a33bd6e7b45e2e2619a10
Brian J Brennan <brianloveswords@gmail.com>
2013-12-18 15:39:15 -0500

    Rename issuer.sql -> schema.sql, add test-data.sql

---
+++ test/test-data.sql
@@ -0,0 +1,13 @@
+INSERT INTO `issuers` SET
+  `slug` = 'chicago',
+  `name` = 'Chicago',
+  `url` = 'http://cityofchicago.org',
+  `description` = 'The City of Chicago',
+  `email` = 'mayor-emanuel@cityofchicago.org';
+
+INSERT INTO `issuers` SET
+  `slug` = 'pittsburgh',
+  `name` = 'Pittsburgh',
+  `url` = 'http://pittsburghpa.gov',
+  `description` = 'The City of Pittsburgh',
+  `email` = 'mayor-ravenstahl@pittsburghpa.gov';
