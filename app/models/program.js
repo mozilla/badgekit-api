90794d5ab82ac3817bfd15e40b85bf9cb73a4b52
Brian J Brennan <brianloveswords@gmail.com>
2014-01-10 11:36:20 -0500

    Add program model.

---
+++ app/models/program.js
@@ -0,0 +1,58 @@
+const check = require('validator').check;
+const db = require('../lib/db');
+
+const Programs = db.table('programs', {
+  fields: [
+    'id',
+    'slug',
+    'name',
+    'description',
+    'issuerId',
+  ],
+  relationships: {
+    image: {
+      type: 'hasOne',
+      local: 'imageId',
+      foreign: { table: 'images', key: 'id' },
+      optional: true,
+    }
+  }
+});
+
+Programs.validateRow = function (row) {
+  return this.fields.reduce(function (errors, field) {
+    try {
+      const validator = validation[field] || noop;
+      validator(row[field]);
+    }
+    catch(e) {
+      e.field = field;
+      errors.push(e);
+    }
+    return errors;
+  }, []);
+};
+
+const validation = {
+  id: function (id) {
+    if (typeof id == 'undefined') return;
+    check(id).isInt();
+  },
+  slug: function (slug) {
+    check(slug).len(1, 50);
+  },
+  name: function (name) {
+    check(name).len(1, 255);
+  },
+  description: function (desc) {
+    check(desc).len(1, 255);
+  },
+  imageId: function (id) {
+    if (typeof id == 'undefined') return;
+    check(id).isInt();
+  },
+};
+
+function noop() {}
+
+exports = module.exports = Programs;
