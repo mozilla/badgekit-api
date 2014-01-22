27817ceeaec01d30b3d04ad01ae8f8bc9862f2ed
Brian J Brennan <brianloveswords@gmail.com>
2014-01-22 11:38:47 -0500

    Start testing badges API.

---
+++ test/badges.test.js
@@ -0,0 +1,61 @@
+const test = require('tap').test
+const app = require('../')
+const fs = require('fs')
+const path = require('path')
+const spawn = require('./spawn')
+
+spawn(app).then(function (api) {
+
+  test('get badge list', function (t) {
+    api.get('/badges').then(function (res) {
+      t.ok(res.badges, 'should have badges')
+      t.same(res.badges[0].slug, 'chicago-badge')
+      t.end()
+    })
+  })
+
+  test('add new badge', function (t) {
+    const form = {
+      slug: 'test-badge',
+      name: 'Test Badge',
+      strapline: 'A badge for testing',
+      description: 'Some description, eh',
+      image: stream('test-image.png'),
+    }
+    api.post('/badges', form).then(function (res) {
+      t.same(res.status, 'created')
+      return api.get('/badges/test-badge')
+    }).then(function (res) {
+      t.same(res.badge.name, form.name)
+      t.ok(res.badge.imageUrl.match(/\/images\/.+/), 'should have right image url')
+      t.end()
+    })
+  })
+
+  test('update badge', function (t) {
+    const diff = {
+      name: 'Test Badge, obvi',
+      description: 'it is still a test!',
+    }
+    api.put('/badges/test-badge', diff).then(function (res) {
+      console.dir(res)
+      t.same(res.status, 'updated')
+    //   return api.get('/programs/test-badge')
+    // }).then(function (res) {
+    //   t.same(res.badge.name, diff.name)
+    //   t.same(res.badge.description, diff.description)
+      t.end()
+    })
+  })
+
+
+
+  test(':cleanup:', function (t) {
+    api.done(); t.end()
+  })
+
+})
+
+function stream(file) {
+  return fs.createReadStream(path.join(__dirname, file))
+}
