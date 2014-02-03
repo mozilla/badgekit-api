e3346c0d2a061e88d3d658003df7efd8c01111f9
Brian J Brennan <brianloveswords@gmail.com>
2014-02-03 09:18:13 -0500

    System schema.

--- schema.sql
+++ schema.sql
@@ -5,6 +5,19 @@ CREATE TABLE `system` (
 ) CHARACTER SET utf8
   ENGINE=InnoDB;
 
+DROP TABLE IF EXISTS `systems`;
+CREATE TABLE `systems` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `slug` VARCHAR(50) NOT NULL UNIQUE,
+  `name` VARCHAR(255) NOT NULL,
+  `url` VARCHAR(255) NOT NULL,
+  `description` TEXT NULL,
+  `email` VARCHAR(255) NULL,
+  `imageId` INT NULL REFERENCES `images`(`id`),
+  PRIMARY KEY (`id`)
+) CHARACTER SET utf8
+  ENGINE=InnoDB;
+
 DROP TABLE IF EXISTS `issuers`;
 CREATE TABLE `issuers` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -18,6 +31,20 @@ CREATE TABLE `issuers` (
 ) CHARACTER SET utf8
   ENGINE=InnoDB;
 
+DROP TABLE IF EXISTS `programs`;
+CREATE TABLE `programs` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `slug` VARCHAR(50) NOT NULL UNIQUE,
+  `name` VARCHAR(255) NOT NULL,
+  `url` VARCHAR(255) NOT NULL,
+  `description` TEXT NULL,
+  `email` VARCHAR(255) NULL,
+  `imageId` INT NOT NULL REFERENCES `images`(`id`),
+  PRIMARY KEY (`id`)
+) CHARACTER SET utf8
+  ENGINE=InnoDB;
+
+
 DROP TABLE IF EXISTS `badges`;
 CREATE TABLE `badges` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -28,19 +55,6 @@ CREATE TABLE `badges` (
   `imageId` INT NOT NULL REFERENCES `images`(`id`),
   `issuerId` INT NULL REFERENCES `issuers`(`id`),
   `archived` BOOLEAN NOT NULL DEFAULT FALSE,
-  PRIMARY KEY (`id`)
-) CHARACTER SET utf8
-  ENGINE=InnoDB;
-
-DROP TABLE IF EXISTS `programs`;
-CREATE TABLE `programs` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `slug` VARCHAR(50) NOT NULL UNIQUE,
-  `name` VARCHAR(255) NOT NULL,
-  `description` TEXT NOT NULL,
-  `url` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NULL,
-  `imageId` INT NOT NULL REFERENCES `images`(`id`),
   PRIMARY KEY (`id`)
 ) CHARACTER SET utf8
   ENGINE=InnoDB;
