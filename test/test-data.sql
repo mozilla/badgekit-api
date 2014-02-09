a4d6878c4211f7b6eda981418a05eaa641f6dab2
Brian J Brennan <brianloveswords@gmail.com>
2014-02-09 13:25:39 -0500

    Fix test data.

--- test/test-data.sql
+++ test/test-data.sql
@@ -1,11 +1,22 @@
 INSERT INTO `systems` SET
+  `id` = 1,
   `slug` = 'chicago',
   `name` = 'Chicago',
   `url` = 'http://cityofchicago.org',
   `description` = 'The City of Chicago',
   `email` = 'mayor-emanuel@cityofchicago.org';
 
+INSERT INTO `systems` SET
+  `id` = 2,
+  `slug` = 'pittsburgh',
+  `name` = 'Pittsburgh',
+  `url` = 'http://pittsburghpa.gov',
+  `description` = 'The City of Pittsburgh',
+  `email` = 'mayor-ravenstahl@pittsburghpa.gov';
+
 INSERT INTO `issuers` SET
+  `id` = 1,
+  `systemId` = 1,
   `slug` = 'chicago-library',
   `name` = 'Chicago Public Library',
   `url` = 'http://www.chipublib.org/',
@@ -13,29 +24,16 @@ INSERT INTO `issuers` SET
   `email` = 'eratosthenes@chipublib.org';
 
 INSERT INTO `issuers` SET
-  `slug` = 'pittsburgh',
-  `name` = 'Pittsburgh',
-  `url` = 'http://pittsburghpa.gov',
-  `description` = 'The City of Pittsburgh',
-  `email` = 'mayor-ravenstahl@pittsburghpa.gov';
-
-INSERT INTO `badges` SET
-  `slug` = 'chicago-badge',
-  `name` = 'Chicago Badge',
-  `strapline` = 'A badge for Chicago';
-
-INSERT INTO `badges` SET
-  `slug` = 'pittsburgh-badge',
-  `name` = 'Pittsburgh Badge',
-  `strapline` = 'A badge for Pittsburgh';
-
-INSERT INTO `badges` SET
-  `slug` = 'archived-badge',
-  `name` = 'Archived Badge',
-  `strapline` = 'An archived badge',
-  `archived` = 1;
+  `id` = 2,
+  `systemId` = 2,
+  `slug` = 'carnegie-library',
+  `name` = 'Carnegie Library of Pittsburgh',
+  `url` = 'http://carnegielibrary.org',
+  `description` = 'Carnegie Library of Pittsburgh',
+  `email` = 'jack-gilbert@carnegielibrary.org';
 
 INSERT INTO `programs` SET
+  `id` = 1,
   `slug` = 'mit-scratch',
   `name` = 'MIT Scratch',
   `url` = 'http://scratch.mit.edu/',
@@ -43,8 +41,30 @@ INSERT INTO `programs` SET
   `email` = 'admin@scratch.mit.edu';
 
 INSERT INTO `programs` SET
+  `id` = 2,
   `slug` = 'khan-academy',
   `name` = 'Khan Academy',
   `url` = 'https://www.khanacademy.org/',
   `description` = 'Start learning now',
   `email` = 'admin@khanacademy.org';
+
+INSERT INTO `badges` SET
+  `id` = 1,
+  `issuerId` = 1,
+  `slug` = 'chicago-badge',
+  `name` = 'Chicago Badge',
+  `strapline` = 'A badge for Chicago';
+
+INSERT INTO `badges` SET
+  `id` = 2,
+  `systemId` = 2,
+  `slug` = 'pittsburgh-badge',
+  `name` = 'Pittsburgh Badge',
+  `strapline` = 'A badge for Pittsburgh';
+
+INSERT INTO `badges` SET
+  `id` = 3,
+  `slug` = 'archived-badge',
+  `name` = 'Archived Badge',
+  `strapline` = 'An archived badge',
+  `archived` = 1;
