2d742b8caa8751fc606bd2554edca7b1768d86bf
Brian J Brennan <brianloveswords@gmail.com>
2014-02-20 17:06:54 -0500

    Abstract finder creation.

--- app/lib/middleware.js
+++ app/lib/middleware.js
@@ -1,36 +1,42 @@
 module.exports = {
-  findSystem: findSystem,
+  findSystem: createFinder('system'),
+  findIssuer: createFinder('issuer'),
 }
 
 const log = require('../lib/logger')
 const restify = require('restify')
-const Systems = require('../models/system')
+const Models = {
+  system: require('../models/system'),
+  issuer: require('../models/issuer')
+}
 
 const http404 = restify.ResourceNotFoundError
 const http500 = restify.InternalError
 
-function findSystem(opts) {
-  opts = opts || {}
-  const param = opts.param || 'systemSlug'
-  const key = opts.key || 'system'
-  const relationships = typeof opts.relationships !== 'undefined'
-    ? opts.relationships
-    : false
-  return function finder(req, res, next) {
-    const slug = req.params[param]
-    const query = {slug: slug}
-    const opts = {relationships: relationships}
-    Systems.getOne(query, opts, function (error, system) {
-      if (error) {
-        log.error(error)
-        return next(new http500('An internal error occured'))
-      }
-      if (!system) {
-        log.warn({code: 'ResourceNotFound', model: 'system', slug: slug})
-        return next(new http404('Could not find system `'+slug+'`'))
-      }
-      req[key] = system
-      return next()
-    })
+function createFinder(modelName) {
+  return function findIssuer(opts) {
+    opts = opts || {}
+    const param = opts.param || modelName + 'Slug'
+    const key = opts.key || modelName
+    const relationships = typeof opts.relationships !== 'undefined'
+      ? opts.relationships
+      : false
+    return function finder(req, res, next) {
+      const slug = req.params[param]
+      const query = {slug: slug}
+      const opts = {relationships: relationships}
+      Models[modelName].getOne(query, opts, function (error, issuer) {
+        if (error) {
+          log.error(error)
+          return next(new http500('An internal error occured'))
+        }
+        if (!issuer) {
+          log.warn({code: 'ResourceNotFound', model: modelName, slug: slug})
+          return next(new http404('Could not find '+modelName+' `'+slug+'`'))
+        }
+        req[key] = issuer
+        return next()
+      })
+    }
   }
 }
