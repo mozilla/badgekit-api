1c666579be85520c16db3b34eae94af36a241d44
Brian J Brennan <brianloveswords@gmail.com>
2014-02-27 14:54:40 -0500

    This one's for JP

--- app/lib/db.js
+++ app/lib/db.js
@@ -2,6 +2,7 @@ var streamsql = require('streamsql');
 
 var options = {
   driver: 'mysql',
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
