2eb921052d99a2b16b267fa2a4c2e236ed0535eb
Erik Christensen <christensenep@gmail.com>
2014-03-03 10:17:55 -0800

    Changes to work with heroku

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,4 +1,5 @@
 var streamsql = require('streamsql');
+var mysql = require('mysql');
 
 var options = {
   driver: 'mysql',
@@ -9,5 +10,32 @@ var options = {
 }
 
 var db = streamsql.connect(options);
+
+function handleDisconnect() {
+  db.connection = mysql.createConnection(options);
+  db.connection.connect(function(err) {
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+    db.query = db.driver.getQueryFn(db.connection);
+    db.queryStream = db.driver.getStreamFn(db.connection);
+  });   
+
+  setErrorHandler();
+}
+
+function setErrorHandler() {
+  db.connection.on('error', function(err) {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    }
+    else {
+      console.error(err);
+    }
+  });
+}
+
+setErrorHandler();  
 
 exports = module.exports = db;
