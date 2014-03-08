cf679da42a4a25d058c9ced158ddeba4e14c01ce
Brian J Brennan <brianloveswords@gmail.com>
2014-03-08 10:37:46 -0500

    Remove unnecessary event emitter from test-webhook-server.js

--- test/test-webhook-server.js
+++ test/test-webhook-server.js
@@ -1,12 +1,9 @@
 const Webhooks = require('../app/models/webhook')
 const http = require('http')
 const Promise = require('bluebird')
-const EventEmitter =  require('events').EventEmitter
 
 module.exports = function makeServer(data) {
-  const emitter = new EventEmitter
   const deferred = Promise.defer()
-
   const server = http.createServer().listen(0)
   server.on('listening', function () {
     const serverUrl = 'http://127.0.0.1:' + this.address().port
